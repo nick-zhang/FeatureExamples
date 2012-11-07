52834a18f4bb5033dbe83447275d90637fcddc93
Zhang Yue <zhangyue02135@yahoo.com.cn>
2012-11-07 17:16:52 +0800

    Created first Linq test case

--- FeatureExamplesTest/LinqTest.cs
+++ FeatureExamplesTest/LinqTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,7 +11,23 @@ namespace FeatureExamplesTest
         [TestMethod]
         public void TestMethod1()
         {
+            string[] names1 = { "nick", "david", "carry" };
+            string[] names2 = { "carry", "david" };
+            var selectedNames = new List<string>();
 
+            foreach (var name in names2)
+            {
+                if (names1.Contains(name))
+                {
+                    selectedNames.Add(name);
+                }
+            }
+
+            
+            foreach (var name in selectedNames)
+            {
+                Console.Out.WriteLine();
+            }
         }
     }
 }
