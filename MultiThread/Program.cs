1ea662f485717c0a2e7d5b4c79f8bc9e4b1fe6d2
Zhang Yue <zhangyue02135@yahoo.com.cn>
2013-02-28 23:38:07 +0800

    Added test for start thread with parameter.

--- MultiThread/Program.cs
+++ MultiThread/Program.cs
@@ -4,7 +4,7 @@ using System.Threading;
 
 namespace MultiThread
 {
-    internal  class Program
+    internal class Program
     {
         private const int INTERVAL = 200;
 
@@ -13,6 +13,13 @@ namespace MultiThread
             var program = new Program();
             var nonParameterThread = new Thread(new ThreadStart(program.NonParameterRun));
             nonParameterThread.Start();
+
+            var parameterizedThread1 = new Thread(program.ParameterRun) {Name = "Thread A"};
+            parameterizedThread1.Start(300);
+
+            var parameterizedThread2 = new Thread(program.ParameterRun) {Name = "Thread B:"};
+            parameterizedThread2.Start(150);
+
         }
 
         /// <summary>  
@@ -24,6 +31,18 @@ namespace MultiThread
             {
                 Console.WriteLine("系统当前时间毫秒值：" + DateTime.Now.Millisecond.ToString(CultureInfo.InvariantCulture));
                 Thread.Sleep(INTERVAL); //让线程暂停  
+            }
+        }
+
+        private void ParameterRun(object ms)
+        {
+            int j;
+            int.TryParse(ms.ToString(), out j); //这里采用了TryParse方法，避免不能转换时出现异常  
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + "系统当前时间毫秒值：" +
+                                  DateTime.Now.Millisecond.ToString(CultureInfo.InvariantCulture));
+                Thread.Sleep(j); //让线程暂停  
             }
         }
     }
